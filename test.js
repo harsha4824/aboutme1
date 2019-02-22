d974fb0daea9b2e91e595c1c3ebb0d012eb32a1b
harsha <harsha4824@gmail.com>
2019-02-22 12:39:10 -0600

    test

--- test.js
+++ test.js
@@ -1,7 +1,8 @@
 QUnit.test('Testing ', function (assert) {
-    assert.equal(speedConverter1(2),3.2,'Tested the conversion with 2 MPH');
+    
     assert.equal(speedConverter1(23),36.800000000000004,'Tested the conversion with 23 MPH');
     assert.equal(speedConverter1(0),0,'Tested the conversion with 0 MPH');
     assert.equal(speedConverter1(-10),-16,'Tested with negative value');
     assert.equal(speedConverter1(5.5),8.8,'Tested the conversion with 5.5 MPH');
+    assert.equal(speedConverter1(1),1.6,'Tested the conversion with 1 MPH');
  });
